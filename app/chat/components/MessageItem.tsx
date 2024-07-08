39ce41de40dea8913a284e0f108e4bc8d9b945de
Edilson R Lima <edilsonlm217@hotmail.com>
2024-07-08 15:05:37 -0300

    refactor: Refactor MessageItem component to render Markdown for bot messages

--- app/chat/components/MessageItem.tsx
+++ app/chat/components/MessageItem.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import remarkGfm from 'remark-gfm';
+import ReactMarkdown from 'react-markdown';
 import clsx from 'clsx';
 
-import { Message } from '../types/message.types';
+import { Message, Sender } from '../types/message.types';
 
 type MessageItemProps = {
   message: Message;
@@ -12,13 +14,19 @@ const MessageItem: React.FC<MessageItemProps> = ({ message }) => (
     className={clsx(
       'mb-2 p-2 rounded-lg max-w-[75%]',
       {
-        'text-white bg-gray-900 self-end ml-auto': message.sender === 'user',
-        'text-white bg-gray-950 self-start': message.sender !== 'user',
+        'text-white bg-gray-900 self-end ml-auto': message.sender === Sender.User,
+        'text-white bg-gray-950 self-start': message.sender === Sender.Bot,
       },
       'whitespace-pre-wrap break-words'
     )}
   >
-    {message.text}
+    {message.sender === 'bot' ? (
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>
+        {message.text}
+      </ReactMarkdown>
+    ) : (
+      message.text
+    )}
   </div>
 );
 
