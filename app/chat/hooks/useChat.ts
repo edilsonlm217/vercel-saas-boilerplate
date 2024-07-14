7e9d039ef56ad4f1d1ec3ada127d3aaaef467bdb
Edilson R Lima <edilsonlm217@hotmail.com>
2024-07-13 23:38:52 -0300

    refactor: Refactor chat functionality to use ChatHook interface

--- app/chat/hooks/useChat.ts
+++ app/chat/hooks/useChat.ts
@@ -4,41 +4,14 @@ import useMessageManager from '../hooks/useMessageManager';
 import useAgentExecutor from './useAgentExecutor';
 
 import { Sender } from '../types/sender.enum';
-import { Message } from '../types/message.types';
+import { ChatHook } from '../types/chat.hook.interface';
 
-interface ChatHook {
-  messages: Message[];
-  message: string;
-  handleInput: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
-  handleKeyDown: (e: React.KeyboardEvent<HTMLTextAreaElement>) => void;
-  sendMessage: () => void;
-  isMessageEmpty: boolean;
-  isStreaming: boolean;
-  handleStop: () => void;
-}
-
-/**
- * Custom hook for chat functionality.
- * 
- * @returns {Object} Hook returns the chat state and handlers.
- * @property {Array} messages - List of messages.
- * @property {string} message - Current message input value.
- * @property {function} handleInput - Handler for input change event.
- * @property {function} handleKeyDown - Handler for key down event.
- * @property {function} sendMessage - Function to send the current message.
- * @property {boolean} isMessageEmpty - Boolean indicating if the current message is empty after trimming.
- * @property {boolean} isStreaming - Boolean indicating if the message is being streamed.
- * @property {function} handleStop - Function to stop streaming.
- */
 const useChat = (): ChatHook => {
   const { messages, addMessage } = useMessageManager();
   const [message, setMessage] = useState('');
   const [isStreaming, setIsStreaming] = useState(false);
   const { sendMessage: sendAgentMessage } = useAgentExecutor();
 
-  /**
-   * Sends the current message if it is not empty.
-   */
   const sendMessage = useCallback(() => {
     const trimmedMessage = message.trim();
     if (trimmedMessage) {
@@ -58,18 +31,10 @@ const useChat = (): ChatHook => {
     }
   }, [message, addMessage, sendAgentMessage]);
 
-  /**
-   * Handler for input change event.
-   * @param {React.ChangeEvent<HTMLTextAreaElement>} e - The change event.
-   */
   const handleInput = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.target.value);
   }, []);
 
-  /**
-   * Handler for key down event.
-   * @param {React.KeyboardEvent<HTMLTextAreaElement>} e - The key down event.
-   */
   const handleKeyDown = useCallback((e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key === 'Enter' && !e.shiftKey) {
       e.preventDefault();
