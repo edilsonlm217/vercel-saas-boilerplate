6555ccd11bf9be095217022485de5d7e90648b5c
Edilson R Lima <edilsonlm217@hotmail.com>
2024-07-14 17:53:25 -0300

    refactor: Refactor UserMessage component to use type definition for props

--- app/chat/components/ChatMessage/UserMessage.tsx
+++ app/chat/components/ChatMessage/UserMessage.tsx
@@ -1,6 +1,10 @@
 import clsx from "clsx";
 
-const UserMessage: React.FC<{ text: string }> = ({ text }) => (
+type UserMessageProps = {
+  text: string;
+};
+
+const UserMessage: React.FC<UserMessageProps> = ({ text }) => (
   <div className={clsx('mb-2 p-2 rounded-lg text-white bg-gray-900 self-end ml-auto max-w-[75%]')}>
     {text}
   </div>
