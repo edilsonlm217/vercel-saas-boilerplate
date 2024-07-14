3181f0a47464e8587bea6ab92e0b6e4f10e3fa6d
Edilson R Lima <edilsonlm217@hotmail.com>
2024-07-14 17:41:35 -0300

    refactor: Refactor chat component imports for consistency and clarity

--- app/chat/components/index.ts
+++ app/chat/components/index.ts
@@ -1,13 +1,13 @@
-import ChatContainer from "./ChatConatiner";
 import ChatForm from "./ChatForm";
 import ChatInput from "./ChatInput";
+import ChatContainer from "./ChatConatiner";
 import ChatMessageList from "./ChatMessageList";
 import ChatSubmitButton from "./ChatSubmitButton";
 
 export {
-  ChatContainer,
   ChatForm,
   ChatInput,
+  ChatContainer,
   ChatMessageList,
-  ChatSubmitButton
+  ChatSubmitButton,
 }
