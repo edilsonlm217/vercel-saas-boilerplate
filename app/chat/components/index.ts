b1cb5d6f49f7c17e45ab72ab92c5063767eff2cd
Edilson R Lima <edilsonlm217@hotmail.com>
2024-07-14 17:11:32 -0300

    feat: Add chat components exports for easier reusability

---
+++ app/chat/components/index.ts
@@ -0,0 +1,13 @@
+import ChatContainer from "./ChatConatiner";
+import ChatForm from "./ChatForm";
+import ChatInput from "./ChatInput";
+import ChatMessageList from "./ChatMessageList";
+import ChatSubmitButton from "./ChatSubmitButton";
+
+export {
+  ChatContainer,
+  ChatForm,
+  ChatInput,
+  ChatMessageList,
+  ChatSubmitButton
+}
