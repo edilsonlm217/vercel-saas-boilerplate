069175687d3312570487d5e77b11c67a96642d38
Edilson R Lima <edilsonlm217@hotmail.com>
2024-07-14 19:05:35 -0300

    refactor: Refactor ChatMessageList structure and add scroll button functionality

---
+++ app/chat/components/ChatMessageList/ChatMessageList.tsx
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react';
+import useScrollToBottom from './hook/useScrollToBottom';
+
+import { Message } from '../../types/message.types';
+
+import ChatMessage from "../ChatMessage";
+import ScrollButton from '@/components/ui/ScrollButton';
+
+interface ChatMessageListProps {
+  messages: Message[];
+}
+
+const ChatMessageList: React.FC<ChatMessageListProps> = ({ messages }) => {
+  const messagesContainerRef = useRef<HTMLDivElement>(null);
+  const { showScrollButton, scrollToBottomClick } = useScrollToBottom(messagesContainerRef, messages);
+
+  return (
+    <>
+      <div ref={messagesContainerRef} className="flex-grow overflow-y-auto px-4 xl:px-0 py-4 md:py-4 bg-black-900 flex flex-col-reverse">
+        {messages.slice(0).reverse().map((msg, index) => (
+          <ChatMessage key={index} message={msg} />
+        ))}
+      </div>
+      {showScrollButton && (
+        <ScrollButton
+          className="absolute bottom-[6rem] left-1/2 transform -translate-x-1/2 bg-gray-500 hover:bg-gray-400 text-white rounded-full p-2 shadow-md"
+          onClick={scrollToBottomClick}
+        />
+      )}
+    </>
+  );
+};
+
+export default ChatMessageList;
---
+++ app/chat/components/ChatMessageList/hook/useScrollToBottom.ts
@@ -0,0 +1,45 @@
+import { Message } from '../../../types/message.types';
+import { useEffect, useState } from 'react';
+
+const useScrollToBottom = (scrollContainerRef: React.RefObject<HTMLDivElement>, messages: Message[]) => {
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  const scrollToBottom = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollTo({
+        top: scrollContainerRef.current.scrollHeight,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const handleScroll = () => {
+    if (scrollContainerRef.current) {
+      const { scrollTop, clientHeight, scrollHeight } = scrollContainerRef.current;
+      const isAtBottom = scrollTop + clientHeight >= scrollHeight - 10;
+      setShowScrollButton(!isAtBottom);
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  useEffect(() => {
+    const container = scrollContainerRef.current;
+    if (container) {
+      container.addEventListener('scroll', handleScroll);
+      return () => {
+        container.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, []);
+
+  const scrollToBottomClick = () => {
+    scrollToBottom();
+  };
+
+  return { showScrollButton, scrollToBottomClick };
+};
+
+export default useScrollToBottom;
