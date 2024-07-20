bb3e0b7f16846ca1a99f15cd718a106c0ab64051
Edilson R Lima <edilsonlm217@hotmail.com>
2024-07-20 17:43:16 -0300

    refactor: Refactor agent setup to implement state graph for tool invocation

--- utils/financial-agent/agentSetup.ts
+++ utils/financial-agent/agentSetup.ts
@@ -1,45 +1,90 @@
-import { ChatOpenAI } from "@langchain/openai";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { AgentExecutor, createToolCallingAgent } from "langchain/agents";
-import { DynamicStructuredTool } from '@langchain/core/tools';
+import { AIMessage, BaseMessage } from "@langchain/core/messages";
+import { DynamicStructuredTool } from "@langchain/core/tools";
+import { StateGraphArgs, END, START, StateGraph, MemorySaver } from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+
 import { z } from "zod";
 
-const multiplyTool = new DynamicStructuredTool({
-  name: "multiply",
-  description: "multiply two numbers together",
-  schema: z.object({
-    a: z.number().describe("the first number to multiply"),
-    b: z.number().describe("the second number to multiply"),
-  }),
-  func: async ({ a, b }: { a: number; b: number }) => {
-    return (a * b).toString();
-  },
-});
+import { ChatOpenAI } from "@langchain/openai";
+import { RunnableConfig } from "@langchain/core/runnables";
 
-const toolsSchema = z.object({
-  a: z.number(),
-  b: z.number(),
-});
+interface IState {
+  messages: BaseMessage[];
+}
 
-export const createAgentExecutor = async () => {
-  const llm = new ChatOpenAI({
-    model: "gpt-4",
-    temperature: 1,
-    apiKey: "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"
+export const createAgentGraph = async () => {
+  // This defines the agent state
+  const graphState: StateGraphArgs<IState>["channels"] = {
+    messages: {
+      value: (x: BaseMessage[], y: BaseMessage[]) => x.concat(y),
+      default: () => [],
+    },
+  };
+
+  const searchTool = new DynamicStructuredTool({
+    name: "search",
+    description: "Call to surf the web.",
+    schema: z.object({
+      query: z.string().describe("The query to use in your search."),
+    }),
+    func: async ({ }: { query: string }) => {
+      // This is a placeholder for the actual implementation
+      // Don't let the LLM know this though 😊
+      return "It's sunny in San Francisco, but you better look out if you're a Gemini 😈.";
+    },
   });
 
-  const prompt = ChatPromptTemplate.fromMessages([
-    ["system", "You are a helpful assistant"],
-    ["placeholder", "{chat_history}"],
-    ["human", "{input}"],
-    ["placeholder", "{agent_scratchpad}"],
-  ]);
+  const tools = [searchTool];
 
-  const tools: DynamicStructuredTool<typeof toolsSchema>[] = [multiplyTool];
+  const toolNode = new ToolNode<{ messages: BaseMessage[] }>(tools);
 
-  const agent = createToolCallingAgent({ llm, tools, prompt });
+  const model = new ChatOpenAI({
+    model: "gpt-4",
+    temperature: 0,
+    openAIApiKey: "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"
+  });
 
-  const executor = new AgentExecutor({ agent, tools });
+  // After we've done this, we should make sure the model knows that it has these tools available to call.
+  // We can do this by binding the tools to the model class.
+  const boundModel = model.bindTools(tools);
 
-  return executor;
-};
+  const routeMessage = (state: IState) => {
+    const { messages } = state;
+    const lastMessage = messages[messages.length - 1] as AIMessage;
+    // If no tools are called, we can finish (respond to the user)
+    if (!lastMessage?.tool_calls?.length) {
+      return END;
+    }
+    // Otherwise if there is, we continue and call the tools
+    return "tools";
+  };
+
+  const callModel = async (
+    state: IState,
+    config?: RunnableConfig,
+  ) => {
+    const { messages } = state;
+    const response = await boundModel.invoke(messages, config);
+    return { messages: [response] };
+  };
+
+  const workflow = new StateGraph<IState>({
+    channels: graphState,
+  })
+    .addNode("agent", callModel)
+    .addNode("tools", toolNode)
+    .addEdge(START, "agent")
+    .addConditionalEdges("agent", routeMessage)
+    .addEdge("tools", "agent");
+
+  // **Persistence**
+  // Human-in-the-loop workflows require a checkpointer to ensure
+  // nothing is lost between interactions
+  const checkpointer = new MemorySaver();
+
+  // **Interrupt**
+  // To always interrupt before a particular node, pass the name of the node to `interruptBefore` when compiling.
+  const graph = workflow.compile({ checkpointer, interruptBefore: ["tools"] });
+
+  return graph;
+}
