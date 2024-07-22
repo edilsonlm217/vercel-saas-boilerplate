20dbf7dfe6ae3f0c93de4d3184afc132b42e819a
Edilson R Lima <edilsonlm217@hotmail.com>
2024-07-22 00:38:21 -0300

    refactor: Refactor ChatMessageList to remove unnecessary useEffect import

--- components/ui/ChatComponents/ChatMessageList/ChatMessageList.tsx
+++ components/ui/ChatComponents/ChatMessageList/ChatMessageList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import useScrollToBottom from '@/hooks/useScrollToBottom';
 import ScrollButton from '@/components/ui/ScrollButton';
 import ChatMessage from "@/components/ui/ChatComponents/ChatMessage";
